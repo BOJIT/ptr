22dfc02c6c5192e5577645e9a6fe9098f5ae59b4
BOJIT <35117353+BOJIT@users.noreply.github.com>
2023-04-11 20:48:03 +0100

    Add email name generator

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -20,10 +20,50 @@ import { sendEmail } from "$lib/email";
 
 /*------------------------------- Functions ----------------------------------*/
 
+const generateRandomDOB = (): string => {
+
+}
+
+function getRandomDate(from: Date, to: Date) {
+    const fromTime = from.getTime();
+    const toTime = to.getTime();
+    return new Date(fromTime + Math.random() * (toTime - fromTime));
+}
+
+async function randomName() {
+    let url = "https://en.wikipedia.org/w/api.php";
+
+    // Timestamp used for range selection
+    const random = getRandomDate(new Date('1999-02-12T01:57:45.271Z'), new Date('2022-02-12T01:57:45.271Z'))
+
+    const params: { [key: string]: string } = {
+        action: "query",
+        format: "json",
+        list: "categorymembers",
+        cmsort: 'timestamp',
+        cmstart: random.toISOString(),
+        cmtitle: "Category:Living+people",
+        cmlimit: "10",
+    };
+
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function (key) { url += "&" + key + "=" + params[key]; });
+
+    console.log(url);
+
+    const response = await fetch(url);
+    const payload = await response.json();
+
+    let names = payload.query.categorymembers.map((c: any) => { return c.title });
+    console.log(names);
+}
+
 /*-------------------------------- Exports -----------------------------------*/
 
 export const actions: Actions = {
     default: async (event) => {
+
+        randomName();
 
         sendEmail(
             "noreply@bojit.org",
