30c4b6bccdd917f83ea5a82f623fd2d8ae2aa2e9
BOJIT <35117353+BOJIT@users.noreply.github.com>
2023-04-11 19:09:19 +0100

    Basic template

---
+++ src/routes/+error.svelte
@@ -0,0 +1,49 @@
+<!--
+ * @file +error.svelte
+ * @author James Bennion-Pedley
+ * @brief Generic Error Page
+ * @date 21/01/2023
+ *
+ * @copyright Copyright (c) 2023
+ *
+-->
+
+<script lang='ts'>
+    /*-------------------------------- Imports -------------------------------*/
+
+    import { page } from '$app/stores';
+
+</script>
+
+
+<div class="error">
+    <h1>{$page.status}</h1>
+    {#if $page.error !== null}
+        <h3>{$page.error.message}</h3>
+    {/if}
+</div>
+
+
+<style>
+    .error {
+        height: 100vh;
+
+        display: flex;
+        text-align: center;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    h1 {
+        margin-bottom: -2rem !important;
+    }
+
+    h3 {
+        color: var(--color-gray-700);
+    }
+
+    img {
+        max-height: 13rem;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<!--
+ * @file +layout.svelte
+ * @author James Bennion-Pedley
+ * @brief Main Application Page
+ * @date 11/01/2023
+ *
+ * @copyright Copyright (c) 2023
+ *
+-->
+
+<script lang="ts">
+    /*-------------------------------- Imports -------------------------------*/
+    import { App, Notification } from "@bojit/svelte-components/core";
+    import palette from "$lib/palette";
+</script>
+
+<App theme={palette}>
+    <slot />
+</App>
+
+<Notification />
